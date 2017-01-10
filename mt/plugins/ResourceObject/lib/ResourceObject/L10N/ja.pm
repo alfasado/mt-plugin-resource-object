80f9dbfeaab3abe6d1294b4b9a3442f3fe611f2b
Junnama Noda <junnama@alfasado.jp>
2017-01-10 12:50:17 +0900

    first commit.

---
+++ mt/plugins/ResourceObject/lib/ResourceObject/L10N/ja.pm
@@ -0,0 +1,11 @@
+package ResourceObject::L10N::ja;
+use strict;
+use base 'ResourceObject::L10N';
+
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+    'Output JSON Resource Object of DataAPI.' => 'Data APIのJSONリソースオブジェクトを出力するテンプレート・タグを提供します。',
+    );
+
+1;
---
+++ mt/plugins/ResourceObject/lib/ResourceObject/Tags.pm
@@ -0,0 +1,32 @@
+package ResourceObject::Tags;
+
+use strict;
+use warnings;
+
+{
+    package MT::App::CMS;
+    sub current_api_version { return MT->request( 'data_api_current_version' ) }
+}
+
+sub _hdlr_resource_object {
+    my ( $ctx, $args, $cond ) = @_;
+    my $model = $args->{ model } || $args->{ stash } || 'entry';
+    my $version = $args->{ version } || 3;
+    MT->request( 'data_api_current_version', $version );
+    my $id = $args->{ id };
+    my $obj;
+    if ( $id ) {
+        $obj = MT->model( $model )->load( $id );
+    } else {
+        $obj = $ctx->stash( $model );
+    }
+    if (! $obj ) {
+        return '{}';
+    }
+    require MT::DataAPI::Resource;
+    my $res = MT::DataAPI::Resource->from_object( $obj ) || return '{}';
+    require MT::DataAPI::Format::JSON;
+    return MT::DataAPI::Format::JSON::serialize( $res );
+}
+
+1;
